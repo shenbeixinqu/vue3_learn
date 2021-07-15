3baeea90ce74e98ce6300af39d4068c40613d6dc
carpediem <1090990640@qq.com>
2021-07-15 23:44:49 +0800

    07/15

---
+++ 08_learn_router/src/router/index.js
@@ -0,0 +1,15 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from '../pages/Home.vue'
+import About from '../pages/About.vue'
+
+const routes = [
+    {path:"/home", component:Home},
+    {path:"/about", component:About},
+]
+
+const router = createRouter({
+    routes,
+    history: createWebHashHistory()
+})
+
+export default router
